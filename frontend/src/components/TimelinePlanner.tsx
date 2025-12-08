fc8dc44d739c21f9955dafb5fde746c5fdba74be
hexadecim8 <64814670+hexa-decim8@users.noreply.github.com>
2025-12-08 14:58:00 -0500

    bugfix, parentheses goofed.

--- frontend/src/components/TimelinePlanner.tsx
+++ frontend/src/components/TimelinePlanner.tsx
@@ -396,7 +396,7 @@ export default function TimelinePlanner({
                 )}
               </div>
             );
-          }))}
+          })}
         </div>
 
         {/* Section Brackets */}
